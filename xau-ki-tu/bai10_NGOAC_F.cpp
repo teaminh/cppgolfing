225501b1d7339c212ff0cb6bdc5c2fe53c9f60c8
teaminh <118674108+teaminh@users.noreply.github.com>
2026-01-10 15:38:16 +0700

    Create bai10_NGOAC_F.cpp

---
+++ xau-ki-tu/bai10_NGOAC_F.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    constexpr char INPUTFILE[] = "NGOAC.inp";
+    constexpr char OUTPUTFILE[] = "NGOAC.out";
+    ifstream input(INPUTFILE, ios::in);
+    if (!input.is_open()) {
+        cout << "\n";
+        return 1;
+    }
+    getline(input, s);
+    input.close();
+    ofstream output(OUTPUTFILE, ios::out | ios::trunc);
+    if (!output.is_open()) {
+        cout << "\n";
+        return 1;
+    }
+    if (s.empty()) {
+        output << "DUNG";
+        output.close();
+        return 0;
+    }
+    int demNgoacTrai { 0 };
+    int ngoacTrai{}, ngoacPhai{};
+    const size_t length = s.length();
+    for (size_t i = 0; i < length; i++) {
+        char charAtI = s[i];
+        if (charAtI == '(') {
+            demNgoacTrai++;
+            ngoacTrai++;
+        } else if (charAtI == ')') {
+            ngoacPhai++;
+            if (demNgoacTrai > 0) {
+                demNgoacTrai--;
+            }
+        } else {
+            output << "KHONG HOP LE";
+            output.close();
+            return 0;
+        }
+    }
+    if (ngoacTrai != ngoacPhai) output << "KHONG DUNG";
+    else output << (demNgoacTrai == 0 ? "DUNG" : "KHONG DUNG");
+    output.close();
+    return 0;
+}
