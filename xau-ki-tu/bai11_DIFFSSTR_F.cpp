b36e192a61712242cd52a11957fc0ed856250c21
teaminh <118674108+teaminh@users.noreply.github.com>
2026-01-10 16:42:23 +0700

    Create bai11_DIFFSSTR_F.cpp

---
+++ xau-ki-tu/bai11_DIFFSSTR_F.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    constexpr char INPUTFILE[] = "DIFFSSTR.INP";
+    constexpr char OUTPUTFILE[] = "DIFFSSTR.OUT";
+    string s;
+    int n;
+    ifstream input(INPUTFILE, ios::in);
+    if (!input.is_open()) {
+        cerr << "input error";
+        return 1;
+    }
+    input >> n;
+    input.ignore();
+    getline(input, s);
+    input.close();
+    ofstream output(OUTPUTFILE, ios::out);
+    if (s.empty()) {
+        output << "0\n";
+        output.close();
+        return 0;
+    }
+    size_t l{ 0 };
+    string o{};
+    for (int i = 0; i < n; i++) {
+        char chatAtI = s[i];
+        if (o.find(chatAtI) != string::npos) {
+            o = "";
+            continue;
+        } else o += chatAtI;
+        if (l == 0) l = o.length();
+        if (o.length() > l) l = o.length();
+    }
+    output << l << '\n';
+    output.close();
+    return 0;
+}
