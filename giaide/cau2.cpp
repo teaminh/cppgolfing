2e324c5db9445ddd8e58f1a86aebb3e93f6c97b4
teaminh <118674108+teaminh@users.noreply.github.com>
2026-01-17 15:58:13 +0700

    Create cau2.cpp

---
+++ giaide/cau2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    const string inputFile = "GROCERY.INP";
+    const string outputFile = "GROCERY.OUT";
+    ifstream finput(inputFile, ios::in);
+    ofstream foutput(outputFile, ios::out | ios::trunc);
+    if (!finput.is_open()) {
+        cerr << "input error";
+        return 1;
+    } else if (!foutput.is_open()) {
+        cerr << "output error";
+        return 1;
+    }
+    int n{}, k{};
+    finput >> n;
+    finput >> k;
+    const int mSize = n * n;
+    int items[mSize]{};
+    for (int i = 0; i < mSize; i++) finput >> items[i];
+    int totalValue{};
+    for (int i = 0; i < mSize - 1; i++) {
+        int itemAtI = items[i];
+        for (int j = i + 1; j < mSize; j++) {
+            int itemAtJ = items[j];
+            int tempValue = itemAtI + itemAtJ;
+            if (tempValue > totalValue && tempValue <= k) totalValue = tempValue;
+            if (totalValue == k) break;
+        }
+        if (totalValue == k) break;
+    }
+    foutput << totalValue << '\n';
+    finput.close();
+    foutput.close();
+    return 0;
+}
